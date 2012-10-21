a5b6afdc631ed07c3a8569a1b090f8974a5cdf15
zyo <exinte@gmail.com>
2012-10-22 00:46:35 +0300

    Comments in index.php and GNU license file

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
+///////////////////////////////////////////////////////////////////////////////////
+//  Main CMS module
+//
+//  @package LizzardmanEngine 
+//  @authors Artem Zelinskyi [lizzardman@], ...
+//  @copyright 2012 
+//  @version $Id$
+//  @since 0.0.0
+//  @license http://opensource.org/licenses/gpl-license.php GNU Public License
+//  LizzardmanEngine is free software. This version may have been modified pursuant
+//  to the GNU General Public License, and as distributed it includes or
+//  is derivative of works licensed under the GNU General Public License or
+//  other free or open source software licenses.
+//  See COPYING.txt for copyright notices and details.
+/////////////////////////////////////////////////////////////////////////////////////
 
+    
+// Main engine defines   
 ini_set('display_errors', 'on');
 error_reporting(E_ALL & ~E_NOTICE);
 
